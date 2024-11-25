4c64715a454f9689922ffdd3b9170e2bca7d573d
iraj <iraj.amini@outlook.com>
2024-11-25 15:29:00 -0500

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Pos Order Filter',
+    'version': '17.0.1.0.0',
+    'category': 'Point of Sale/Inventory/Purchase',
+    'summary': 'Pos Order Filter',
+    'description': 'Pos Order Filter',
+    'author': 'Impelement Digital',
+    'depends': ['purchase','stock','point_of_sale'],
+    'data': ['views/quotation_analysis_report.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}
